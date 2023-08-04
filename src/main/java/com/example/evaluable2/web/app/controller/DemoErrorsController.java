c7353ba6bf87fe4244f6aaff2cf70472eaa4746a
jos <elmelnibone@gmail.com>
2023-08-04 09:20:22 +0200

    libros,usuarios, homecontroller y errores

---
+++ src/main/java/com/example/evaluable2/web/app/controller/DemoErrorsController.java
@@ -0,0 +1,30 @@
+package com.example.evaluable2.web.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/error")
+public class DemoErrorsController {
+	
+	@GetMapping("/template-no-existe")
+	public String m1() {
+		return "template-no-existe";
+	}
+	
+	@GetMapping("/division-por-0")
+	public String m2() {
+		int x = 1/0;
+		// como no va a llegar al return, saltará antes el otro error
+		return "template-no existe";
+	}
+	
+	@GetMapping("/null-pointer")
+	public String m3() {
+		String s = null;
+		s.length();
+		// como no va a llegar al return, saltará antes el otro error
+		return "template-no existe";
+	}
+}
--- src/main/java/com/example/evaluable2/web/app/controller/HomeController.java
+++ src/main/java/com/example/evaluable2/web/app/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/home")
+	@GetMapping({"/home", "/"})
 	public String m2(Model model) {
 
 		model.addAttribute("title", "La Biblioteca");
